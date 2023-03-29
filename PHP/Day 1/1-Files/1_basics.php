a9174589f4bbc3f4bd83c04a0270379315f17713
ChVikkk <114808096+turelyk-arsen@users.noreply.github.com>
2023-03-29 10:37:50 +0200

    Initial commit

---
+++ PHP/Day 1/1-Files/1_basics.php
@@ -0,0 +1,29 @@
+<?php
+// This is a single line comment
+/*
+this is 
+a multiline
+comment
+*/
+// 1.Simple Variables
+$firstName = 'Simon';
+$age = 31;
+$height = 1.80;
+$are_you_doing_well = false;
+/*Rules:
+-name of the variable must start with a letter
+-name can contain numbers
+-only underscore, on other symbols ! 
+*/
+
+// echo 'Hello world';
+
+echo $firstName;
+echo '<br>';
+echo $age;
+echo '</br>';
+
+echo "<h1>$firstName</h1>";
+echo '<h1>$firstName</h1>';
+
+?>
