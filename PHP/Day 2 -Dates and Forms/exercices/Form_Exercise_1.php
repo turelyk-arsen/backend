ce807a790cde7aa174d20424fe47dacb5f3875b3
ChVikkk <114808096+turelyk-arsen@users.noreply.github.com>
2023-03-30 12:41:54 +0200

    30/03/2023 lunch

---
+++ PHP/Day 2 -Dates and Forms/exercices/Form_Exercise_1.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+
+<body>
+
+	<?php
+
+	/* 
+-- Exercise 1 :
+	
+	1.1 : 
+	Create a new page with a basic connection form with email and password input.
+
+	1.2 :
+	Get the click of the button.
+
+	1.3 :
+	Check if the mail contains the '@' symbol. 
+	If yes, display 'valid email';
+	If no, display 'invalid email';
+
+	To do this you have to check for "str_pos" function !
+
+	1.4 :
+	When the user validates the form: display a message in red if invalid, show in green if valid.
+echo '<p style="color:red">not valid</p>';
+
+*/
+	// echo '<pre>';
+	// var_dump($_POST);
+	// echo '</pre>';
+
+	// $email = '';
+	if (isset($_GET['button'])) {
+		// $errors = false;
+		$email = $_GET['email'];
+		if (strpos($email, '@')) {
+			// echo 'valid email';
+			echo '<p style="color:green"> valid</p>';
+		} else {
+			// echo 'invalid email';
+			echo '<p style="color:red">not valid</p>';
+		}
+	}
+	?>
+
+	<form action="Form_Exercise_1.php" method="GET">
+		<input type="text" name="email" value="<?php echo $email ?>"><br>
+		<input type="password" name="password"><br>
+		<input type="submit" name="button" value="Login">
+	</form>
+
+</body>
+
+</html>
--- PHP/Day 2 -Dates and Forms/files/3_forms.php
+++ PHP/Day 2 -Dates and Forms/files/3_forms.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form PHP</title>
 </head>
+
 <body>
     <?php
     /*
@@ -27,6 +29,41 @@
 
     action attribute: choose WHERE (which page) to send the date to.
     */
+
+    echo '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+
+    $firstName = '';
+    if (isset($_POST['submitBtn'])) {
+        $errors = false;
+        // saving data coming from the form in the variable
+        $firstName = $_POST['firstname'];
+    }
+    //validation
+    if (empty($firstName)){
+        echo 'Firstname is mandatory <br>';
+        $errors = true;
+    }
+
+    if (empty($_POST['lastname'])) {
+        echo 'Lastname is mandatory <br>';
+        $errors = true;
+    }
+
+    // Only if NO errors
+    if ($errors == false) {
+        echo 'Form was submitted. First name:'.$_POST['firstname'].'/Last name: '.$_POST['lastname'];
+    }
+
+    // end of PHP
     ?>
+    <form method="POST">
+        <input type="text" name="firstname" placeholder="First name" value="<?php echo $firstName; ?>"> <br>
+        <input type="text" name="lastname" placeholder="Last name"> <br>
+        <textarea name="message" cols="15" rows="5"></textarea> <br>
+        <input type="submit" name="submitBtn" value="Send message">
+    </form>
 </body>
+
 </html>
