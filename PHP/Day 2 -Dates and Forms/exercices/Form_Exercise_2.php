a9d4ef1019f9e556ae6f8aaf48d6b7efeb4f1713
ChVikkk <114808096+turelyk-arsen@users.noreply.github.com>
2023-03-30 15:32:53 +0200

    30/03/2023 afternoon

--- PHP/Day 2 -Dates and Forms/exercices/Form_Exercise_2.php
+++ PHP/Day 2 -Dates and Forms/exercices/Form_Exercise_2.php
@@ -1,6 +1,47 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-/* -- Exercise : 
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<style>
+			table,th{
+			border: 1px solid;
+			width: 10%;
+
+		}
+	</style>
+
+</head>
+
+<body>
+	<form method="POST">
+		<label for="">Send NUMBER</label>
+		<input type="integer" name="number">
+		<input type="submit" name="button" value="send">
+	</form>
+
+	<?php
+	if (isset($_POST['button'])) {
+		$number = $_POST['number'];
+		$table = [];
+		if (is_numeric($number)) {
+			for ($i = 1; $i <= 10; $i++) {
+				$table = $i * $number;
+				echo "<table><tr>
+			<th>$number * $i = </th>
+			<th>$table</th>
+			</tr></table>";
+			}
+		} else {
+			echo "<h2> Please write a number </h2>";
+		}
+	}
+
+
+	/* -- Exercise : 
 	2.1 (Already done in previous exercise) :
 	Write a PHP script that gives the multiplication table of 2.
 	Multiplication table from 1 to 10.
@@ -16,3 +57,7 @@
 	2.4 :
 	Display the multiplication table in a nice HTML format table style.
 */
+	?>
+</body>
+
+</html>
---
+++ PHP/Day 2 -Dates and Forms/exercices/signin.php
@@ -0,0 +1,69 @@
+<!--             - Last Name
+	            - First name
+	            - E-mail
+	            - Password
+	            - Confirmation of password
+	            - Checkbox "Subscribe to the newsletter" -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h3>FORM</h3>
+    <form method="POST">
+        <input type="text" name="firstname" placeholder="First name"> <br>
+        <input type="text" name="lastname" placeholder="Last name"><br>
+        <input type="email" name="email" placeholder="Email"> <br>
+        <input type="password" name="password" placeholder="Password"> <br>
+        <input type="password" name="pass_confirm" placeholder="Password confirm"> <br>
+        <input type="checkbox" id="newsletter" name="newsletter" value="newsletter">
+        <label for="newsletter"> Subscribe to the newsletter</label><br>
+        <input type="submit" value="send" name="submit_form">
+    </form>
+
+    <?php
+    if (isset($_POST['submit_form'])) {
+        $firstName = $_POST['firstname'];
+        $lastName = $_POST['lastname'];
+        $email = $_POST['email'];
+        $password =$_POST['password'];
+        $pass_confirm =$_POST['pass_confirm'];
+
+        if (empty($firstName)) {
+            echo "<p>'Firstname is mandatory </p>";
+        } 
+        if (empty($lastName)) {
+            echo "<p>'Lastname is mandatory </p>";
+        } 
+
+        if (8 > strlen($email) ||  strlen($email) > 50) {
+        echo "<p>The e-mail must be between 8 and 50 characters</p>";
+        }
+        
+        if ($password === $pass_confirm) {
+            echo "The fields Password and Confirmation must be identical and have at least 8 characters";
+        }
+        else {
+            echo "<p>First name: $firstName";
+            echo "<p>Last name: $lastName";
+            echo "<p>Email: $email";
+        }
+    }
+
+
+
+
+
+
+    ?>
+
+</body>
+
+</html>
