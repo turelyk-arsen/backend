989e234b25a39ad313859a2ec4a5c3e2d4483b8b
ChVikkk <114808096+turelyk-arsen@users.noreply.github.com>
2023-04-11 13:24:02 +0200

    11/04/2023 lunch2

--- PHP/Project Individual Day/blog_PHP/login.php
+++ PHP/Project Individual Day/blog_PHP/login.php
@@ -1,5 +1,5 @@
 <?php
-include_once "./templates/generation.php";
+  include_once "./templates/generation.php";
 
     $email = '';
     if (isset($_POST['loginBtn'])) {
@@ -45,11 +45,38 @@ include_once "./templates/generation.php";
         header('Location: index.php');
     }
 
-
+    if (isset($_COOKIE['user_name'])) 
+    header('Location: index.php');
     
-
     ?>
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Main page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <?php
+    generation_head_menu($mysqli);
+    ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <?php
+                generation_posts_index($mysqli);
+                ?>
+            </div>
+            <div class="col-2">
     <h2>Login</h2>
+    <a href="index.php">Sign up</a>
+    <br>
     <form method="post">
 
         <label for="email">Email</label>
@@ -66,3 +93,10 @@ include_once "./templates/generation.php";
 
         <input type="submit" name="loginBtn" value="Login">
     </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
