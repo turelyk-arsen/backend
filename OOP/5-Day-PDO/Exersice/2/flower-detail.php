c2e694ba7c390158844678c0f40086d9f63f73c6
ChVikkk <114808096+turelyk-arsen@users.noreply.github.com>
2023-04-21 16:32:29 +0200

    21/04/2023 afternoon

--- OOP/5-Day-PDO/Exersice/2/flower-detail.php
+++ OOP/5-Day-PDO/Exersice/2/flower-detail.php
@@ -1,2 +1,23 @@
 <?php
+require_once 'FlowerManager.php';
 
+$id = $_GET['id'];
+$data = array($id);
+
+$dsn = 'mysql:host=localhost;dbname=flower_db';
+$pdo = new PDO($dsn, 'root', '');
+
+$result = $pdo->prepare('SELECT * FROM flowers WHERE id = ?');
+$result->execute($data);
+
+$flowers = $result->fetch(PDO::FETCH_ASSOC);
+// var_dump($flowers);
+
+$flaw = new FlowerManager($flowers['name'], $flowers['price']);
+echo $flaw->find($id);
+
+// echo $flowers['name'];
+// echo $flowers['price'];
+
+
+$dsn = null;
--- OOP/5-Day-PDO/Exersice/2/view-flowers.php
+++ OOP/5-Day-PDO/Exersice/2/view-flowers.php
@@ -7,12 +7,33 @@ $pdo = new PDO($dsn, 'root', '');
 
 $result = $pdo->query('SELECT * FROM flowers');
 $flowers = $result->fetchAll(PDO::FETCH_ASSOC);
-
-
-foreach ($flowers as $flower) {
-    $fl = new FlowerManager($flower['name'], $flower['price']);
-    echo $fl->findAll();
-    echo "<a href='./flower-detail.php?id=<?= $flower['id']; ?>Detail page</a>";
-}
-
 $dsn = null;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <?php foreach ($flowers as $flower) :
+        $fl = new FlowerManager($flower['name'], $flower['price']);
+        echo $fl->findAll(); ?>
+        <br>
+        <a href="./flower-detail.php?id=<?= $flower['id']; ?>">See more</a>
+
+    <?php endforeach; ?>
+
+
+
+</body>
+
+</html>
