04f125913ebee35e16eee97339f00c298cb6f9cc
ChVikkk <114808096+turelyk-arsen@users.noreply.github.com>
2023-04-27 13:25:20 +0200

    27/04/2023 lunchtime

--- OOP/Revision/2 my rewrite/Post.php
+++ OOP/Revision/2 my rewrite/Post.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'StringIsEmptyOrNull.php';
+
 class Post
 {
     public $id;
@@ -11,17 +13,44 @@ class Post
     // The method should take the title and content of the post 
     // as arguments.
 
+    public static function getPDO($host, $schema, $user, $pwd)
+    {
+        return new PDO('mysql:host' . $host . ';dbname=' . $schema, $user, $pwd);
+    }
+
     public static function createPost($title, $content)
     {
-        $conn = new PDO('mysql:host=localhost;dbname=forum', 'root', '');
 
-        $sql = "INSERT INTO posts (title, content) VALUES (?, ?)";
-        $result = $conn->prepare($sql);
-        $result->bindValue(1, $title);
-        $result->bindValue(2, $content);
+        if ($title == '' or $title == null) {
+            throw new StringIsEmptyOrNull('The title is empty, you must provide a valid title');
+        } else if ($content == '' or $content = null) {
+            throw new StringIsEmptyOrNull('The content is empty, you must provide valid content');
+        }
 
-        $result->execute();
+        $data = array('title' => $title, 'content' => $content);
+        $conn = self::getPDO('localhost', 'forum', 'root', '');
+        $stmt = $conn->prepare("INSERT INTO posts VALUE (id, :title, :content");
+        $stmt->execute($data);
         $conn = null;
+
+        // $conn = new PDO('mysql:host=localhost;dbname=forum', 'root', '');
+
+        // $sql = "INSERT INTO posts (title, content) VALUES (?, ?)";
+        // $result = $conn->prepare($sql);
+        // $result->bindValue(1, $title);
+        // $result->bindValue(2, $content);
+
+        // $result->execute();
+        // $conn = null;
+
+    }
+    public static function getPosts()
+    {
+        $conn = self::getPDO('localhost', 'forum', 'root', '');
+        $stmt = $conn->query('SELECT * FROM posts');
+        $posts = $stmt->fetchAll(PDO::FETCH_CLASS, 'Post');
+        $conn = null;
+        return json_encode($posts, JSON_PRETTY_PRINT);
     }
 }
 
---
+++ OOP/Revision/2 my rewrite/StringIsEmptyOrNull.php
@@ -0,0 +1,5 @@
+<?php
+
+class StringIsEmptyOrNull extends Exception {
+
+}
