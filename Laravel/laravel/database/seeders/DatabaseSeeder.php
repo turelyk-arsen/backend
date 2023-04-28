96d2a1d1dd5ed28f1ea2290f76c2a4c1b93662ee
ChVikkk <114808096+turelyk-arsen@users.noreply.github.com>
2023-04-28 15:13:44 +0200

    28/04/2023 afternoon

--- Laravel/laravel/database/seeders/DatabaseSeeder.php
+++ Laravel/laravel/database/seeders/DatabaseSeeder.php
@@ -15,26 +15,27 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(5)->create();
+        Listing::factory(6)->create();
 
         // lets create a bunch of hard coded stuff
-        Listing::create([
-            'title' => 'Laravel Senior Developer',
-            'tags' => 'laravel, javascript, GPT',
-            'company' => 'Acme Corp',
-            'location' => 'Boston',
-            'email' => 'email1234@email.com',
-            'website' => 'https://www.acme.com',
-            'description' => 'lorem lorem lorem lorem lorem lorem lorem lorem lorem'
-        ]);
-        Listing::create([
-            'title' => 'Fullstack engineer',
-            'tags' => 'laravel, backend',
-            'company' => 'Stark Industries',
-            'location' => 'New York',
-            'email' => 'ema@stark.com',
-            'website' => 'https://www.stark.com',
-            'description' => 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem'
-        ]);
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript, GPT',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston',
+        //     'email' => 'email1234@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'lorem lorem lorem lorem lorem lorem lorem lorem lorem'
+        // ]);
+        // Listing::create([
+        //     'title' => 'Fullstack engineer',
+        //     'tags' => 'laravel, backend',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York',
+        //     'email' => 'ema@stark.com',
+        //     'website' => 'https://www.stark.com',
+        //     'description' => 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem'
+        // ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
